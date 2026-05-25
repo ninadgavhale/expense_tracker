3aac5575078884b31d93226f1c99455814267ce4
ninadgavhale <ninad1301@gmail.com>
2026-05-25 23:16:12 +0530

    added the feature of sum up total value

--- expence tracker (CLI).py
+++ expence tracker (CLI).py
@@ -4,9 +4,23 @@ def expence_tracker():
     while True:
         user_expence = float(input(" amount you spend "))
         description = input("describe , where u spend  ")
-        store.append((user_expence, description))
+        store.append({
+            "ammount":user_expence,
+            "description":description
+        })
         
-    another = input("do you want to add another expence? (yes/no) ")
-    
+        another = input("do you want to add another expence? (yes/no) ").strip().lower()
+        if another == 'yes':
+            pass
+        elif another == "no":
+            print("thank you for using expence tracker")
+            break
+        else:            
+            print("thank you for using expence tracker")
+            break
+    print(f"your total expence is {sum('ammount')}")
+       
+
+
 expence_tracker()
 print(store)
